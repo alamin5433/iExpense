667fb19811568efc0c1835a9236a6ac770dc8815
Alamin <alamin_cse_ru@yahoo.com>
2020-03-28 21:42:27 +0600

    Why @State only works with structs

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Al Amin on 28/3/20.
+//  Copyright © 2020 Al Amin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var user = User()
+    var body: some View {
+        VStack(spacing: 10){
+            Text("Your name is \(user.firstName) \(user.lastName)")
+            
+            TextField("First name", text: $user.firstName)
+                .cornerRadius(10)
+            TextField("Second Name", text: $user.lastName)
+        }
+    .padding()
+    }
+}
+
+class User {
+    var firstName = "Bilbo"
+    var lastName = "Baggins"
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
